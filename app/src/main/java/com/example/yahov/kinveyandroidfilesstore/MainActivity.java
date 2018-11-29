82c6d1705ebcd230f021094192a917a69024f8e4
bilger-progress <bilger.yahov@progress.com>
2018-11-29 17:06:42 +0200

    Implement logic for file upload to Kinvey File Store.

--- app/src/main/java/com/example/yahov/kinveyandroidfilesstore/MainActivity.java
+++ app/src/main/java/com/example/yahov/kinveyandroidfilesstore/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.yahov.kinveyandroidfilesstore;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,9 +9,15 @@ import android.widget.Toast;
 import com.kinvey.android.Client;
 import com.kinvey.android.callback.KinveyPingCallback;
 import com.kinvey.android.model.User;
+import com.kinvey.android.store.FileStore;
 import com.kinvey.android.store.UserStore;
 import com.kinvey.java.core.KinveyClientCallback;
-
+import com.kinvey.android.callback.AsyncUploaderProgressListener;
+import com.kinvey.java.core.MediaHttpUploader;
+import com.kinvey.java.model.FileMetaData;
+import com.kinvey.java.store.StoreType;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     private String kinveyUserPassword = "xxx";
     private Button kinveyLoginButton;
     private Button kinveyUploadFileButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +63,17 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        kinveyUploadFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    KinveyFileUpload();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void KinveyLogin () throws IOException {
@@ -79,6 +96,49 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_LONG).show();
                 System.out.println("User: " + u.toString());
                 kinveyUploadFileButton.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void KinveyFileUpload () throws IOException {
+        // Let's create a sample file.
+        String fileName = "MyFile.txt";
+        String fileContents = "Hello world!";
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            outputStream.write(fileContents.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Let's get the file.
+        File actualFile = new File(getFilesDir().getAbsolutePath() + "/MyFile.txt");
+        // Let's trigger Kinvey File Upload.
+        final boolean isCancelled = false;
+        FileStore fileStore = kinveyClient.getFileStore(StoreType.CACHE);
+        fileStore.upload(actualFile, new AsyncUploaderProgressListener<FileMetaData>() {
+            @Override
+            public void onSuccess(FileMetaData fileMetaData) {
+                Toast.makeText(getApplicationContext(), "File Upload Successful", Toast.LENGTH_LONG).show();
+                System.out.println("File Meta-Data: " + fileMetaData.toString());
+            }
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(getApplicationContext(), "Exception was thrown. Check logs.", Toast.LENGTH_LONG).show();
+                System.out.println("Exception was thrown: " + t.getMessage());
+            }
+            @Override
+            public void progressChanged(MediaHttpUploader mediaHttpUploader) throws IOException {
+                // Place your code here.
+            }
+            @Override
+            public void onCancelled() {
+                // Place your code here.
+            }
+            @Override
+            public boolean isCancelled() {
+                return isCancelled;
             }
         });
     }
